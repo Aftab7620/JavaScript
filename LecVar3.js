a393fe0fa2eb37bd9f2ae5e09f18eb4d6c8de5de
Aftab7620 <mohdaftab7620@gmail.com>
2023-05-22 14:24:14 +0530

    Create LecVar3.js

---
+++ LecVar3.js
@@ -0,0 +1,15 @@
+// Variable In javascript
+
+var X='Aftab';
+console.log(X);
+
+var sum=6+9;
+console.log(sum);
+
+var rest=sum-5;
+console.log(rest);
+
+var Age=prompt("What is your Age");
+var year=prompt("Which year is goin on");
+var final=alert("I was Born In Year:" +(year-Age));
+
