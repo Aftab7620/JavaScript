8ac3a2fa7df53bd0301d767f461d2fad64c4829e
Aftab7620 <mohdaftab7620@gmail.com>
2023-05-22 13:38:58 +0530

    Js Datatype

---
+++ Data.js
@@ -0,0 +1,21 @@
+//Datatype
+//Numeric
+
+console.log(2+6);
+console.log(9-3);
+console.log(4*6);
+console.log(6/2);
+
+//String
+console.log('Aftab');
+console.log('Khan');
+console.log('Aftab ' + ' Khan');
+console.log(21+ ' years')
+console.log(22- '10');
+
+//BOOLEAN TRUE OR FALSE
+console.log(5>4);
+console.log(5<5);
+console.log(5>=5);
+console.log(5<=5);
+
