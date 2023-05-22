91eff8d527706b1ca8e6bba0d3745a47fe6c1031
Aftab7620 <mohdaftab7620@gmail.com>
2023-05-22 14:00:59 +0530

    Exercise 1

---
+++ Ex1.js
@@ -0,0 +1,21 @@
+//EXCLAIMER:
+/*GUESS THE ANSWERS IF YOU CAN'T JUST FIND THE SOLUTIONS FOR THE
+SAME EXERCISE
+USE CONSOLE.LOG IF YOU ARE USING VSCODE TEXT EDITOR OR
+COPY AND PASTE IT DIRECTLY TO THE WEB BROWSER CONSOLE
+
+*/
+
+//DO THE FOLLOWING EVALUATION
+console.log(5+6);
+console.log(6 + '7');
+console.log(8  - '7');
+console.log(10 % 5); 
+console.log(11 % 5); 
+console.log(true + false);
+console.log(false + true);
+console.log(true + true);
+console.log(false + false);
+console.log("I"+"Like"+"YOU!");
+
+console.log("It\'s my turn");
