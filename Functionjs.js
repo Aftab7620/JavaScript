e16a994264a54e99f9552016037a179d5b885495
Aftab7620 <mohdaftab7620@gmail.com>
2023-06-12 20:53:38 +0530

    Create Functionjs.js

---
+++ Functionjs.js
@@ -0,0 +1,17 @@
+//there are 4 type of function in javascript 
+//1) prompt
+//2)alert
+//3)function and call function
+//4)function with argument
+
+function First(){
+    console.log("this is my first function");
+}
+First();
+
+function Addition(Num1,num2)
+{
+    var x=Num1+num2;
+    console.log('Addition :' +x);
+}
+Addition(7,6);
