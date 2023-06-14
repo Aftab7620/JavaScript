a757e2fae1ae83900a3a711d115b9b88503de2fd
Aftab7620 <mohdaftab7620@gmail.com>
2023-06-14 22:12:36 +0530

    Create ArrayEx.js

---
+++ ArrayEx.js
@@ -0,0 +1,12 @@
+//Declaration of an array
+var FirstArray=[1,2,3,4];
+console.log(FirstArray);
+
+//mix array
+var Second=[1,2,'Aftab','Khan'];
+console.log(Second);
+console.log(Second[2]);
+
+var third=[1,[2,7,5,8]];
+console.log(third);
+console.log(third[1][2]);
