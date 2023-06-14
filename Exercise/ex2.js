033294a525d08ec3870535cc27b0cc733cb3a69a
Aftab7620 <mohdaftab7620@gmail.com>
2023-06-14 13:21:00 +0530

    Create ex2.js

---
+++ Exercise/ex2.js
@@ -0,0 +1,30 @@
+//EXCLAIMER:
+/*GUESS THE ANSWERS IF YOU CAN'T JUST FIND THE SOLUTIONS FOR THE
+SAME EXERCISE
+USE CONSOLE.LOG IF YOU ARE USING VSCODE TEXT EDITOR OR
+COPY AND PASTE IT DIRECTLY TO THE WEB BROWSER CONSOLE
+
+*/
+
+//DO THE FOLLOWING
+//1) create a variable called firstName
+//2) assign a value to that variable, preferably your name
+//3) var c? What is c equal to, console it 
+//4) create two variable a and z
+//5) give variable a value 5 and variable z value 3
+//6) what a + z will give us?
+//7) Now give variable a = 1 and do a + z again
+//8) Make calculator that prompts the user for two numbers
+//9) store the values of that two numbers in any variables you want
+//10) Display back with alert their multiplication
+
+var firstname="Aftab";
+console.log(firstname);
+
+var a=5;
+var z=3;
+console.log('Addition of ' +(a+z));
+a=1;
+console.log('Addition of ' +(a+z));
+
+
