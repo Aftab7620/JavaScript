9f5c6495454708a4fe192933ad4688f13ee14dbb
Aftab7620 <mohdaftab7620@gmail.com>
2023-06-21 22:37:12 +0530

    Update user.js

--- Exercise/user.js
+++ Exercise/user.js
@@ -1,3 +1,6 @@
+//take a input from user and change the bg color of body 
+//align the text in middle of the window.
+
 var username=prompt("enter color to be changed");
 document.body.style.background=username;
 
