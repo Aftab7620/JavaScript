253e3467efb4bd4e2b544bf95731797ecef39cbb
Aftab7620 <mohdaftab7620@gmail.com>
2023-07-01 21:56:56 +0530

    Create file1.js

---
+++ module/file1.js
@@ -0,0 +1,13 @@
+ export let name="Aftab Khan";
+
+  export function hello(){
+    console.log("Hello");
+ }
+
+
+
+ export class Aftab{
+    constructor(){
+        console.log("Aftab called");
+    }
+ }
