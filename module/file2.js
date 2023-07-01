682671a75e0e60913571d8b125c60a9f04e6884b
Aftab7620 <mohdaftab7620@gmail.com>
2023-07-01 21:57:12 +0530

    Create file2.js

---
+++ module/file2.js
@@ -0,0 +1,10 @@
+//import {name} from "./file1.js";
+import * as ak from "./file1.js";
+
+console.log(ak.name);
+
+//import {hello} from "./file1.js";
+console.log(ak.hello());
+
+//import { Aftab } from "./file1.js";
+let a=new ak.Aftab();
